361071c31a83f5fdb3158e9cea19d38cddfa104d
mathiasgam <mathias@gammelmark.org>
2020-07-13 17:34:54 +0200

    Added cam movement

--- core/core/Application.cpp
+++ core/core/Application.cpp
@@ -5,6 +5,7 @@
 #include "input/Input.h"
 
 #include "glm.hpp"
+#include "gtc/matrix_transform.hpp"
 #include "glad/glad.h"
 
 #include "scene/Camera.h"
@@ -20,8 +21,12 @@ namespace LSIS {
 	std::unique_ptr<Scene> m_scene;
 	std::shared_ptr<Camera> m_cam;
 
-	glm::vec3 cam_pos = glm::vec3(1, 1, 1);
+	glm::vec4 cam_pos = glm::vec4(1, 1, 1, 1);
+	glm::vec4 cam_vel = glm::vec4(0, 0, 0, 0);
 	glm::vec3 cam_rot = glm::vec3(0, 0, 0);
+
+	float cam_speed = 0.01f;
+	float cam_speed_rot = 0.01f;
 
 	void Application::Init()
 	{
@@ -52,6 +57,13 @@ namespace LSIS {
 		Initialized = true;
 	}
 
+	void UpdateCam() {
+		glm::mat4 R = glm::rotate(glm::rotate(glm::identity<glm::mat4>(), cam_rot.y, glm::vec3(0, 1, 0)), cam_rot.x, glm::vec3(1, 0, 0));
+
+		cam_pos += R * cam_vel * cam_speed;
+		m_cam->SetPosition(cam_pos);
+	}
+
 	void Application::Run()
 	{
 		std::cout << "Running\n";
@@ -63,6 +75,7 @@ namespace LSIS {
 
 		while (!m_window->IsCloseRequested()) {
 			m_window->Clear();
+			UpdateCam();
 
 			m_scene->Render();
 
@@ -70,7 +83,7 @@ namespace LSIS {
 
 			m_window->Update();
 			m_window->SwapBuffers();
-			m_window->WaitForEvent();
+			m_window->PollEvents();
 		}
 	}
 
@@ -100,6 +113,27 @@ namespace LSIS {
 	void Application::OnKeyPressedEvent(const KeyPressedEvent& e)
 	{
 		int key = e.GetKey();
+		switch (key)
+		{
+		case KEY_A:
+			cam_vel.x -= 1.0f;
+			break;
+		case KEY_D:
+			cam_vel.x += 1.0f;
+			break;
+		case KEY_S:
+			cam_vel.z += 1.0f;
+			break;
+		case KEY_W:
+			cam_vel.z -= 1.0f;
+			break;
+		case KEY_SPACE:
+			cam_vel.y += 1.0f;
+			break;
+		case KEY_LEFT_CONTROL:
+			cam_vel.y -= 1.0f;
+			break;
+		}
 
 		std::cout << "Key pressed: " << GetKeyString(key) << std::endl;
 
@@ -108,6 +142,27 @@ namespace LSIS {
 	void Application::OnKeyReleasedEvent(const KeyReleasedEvent& e)
 	{
 		int key = e.GetKey();
+		switch (key)
+		{
+		case KEY_A:
+			cam_vel.x += 1.0f;
+			break;
+		case KEY_D:
+			cam_vel.x -= 1.0f;
+			break;
+		case KEY_S:
+			cam_vel.z -= 1.0f;
+			break;
+		case KEY_W:
+			cam_vel.z += 1.0f;
+			break;
+		case KEY_SPACE:
+			cam_vel.y -= 1.0f;
+			break;
+		case KEY_LEFT_CONTROL:
+			cam_vel.y += 1.0f;
+			break;
+		}
 		std::cout << "Key released: " << GetKeyString(key) << std::endl;
 	}
 
@@ -122,14 +177,15 @@ namespace LSIS {
 
 		if (mods & BIT(0)) {
 			std::cout << "right drag\n";
-		}else if (mods & BIT(1)) {
+		}
+		else if (mods & BIT(1)) {
 			const float diffX = x - lastX;
 			const float diffY = y - lastY;
 			cam_rot.y -= diffX * 0.01f;
 			cam_rot.x = glm::clamp(cam_rot.x - diffY * 0.01f, -3.14f, 3.14f);
 
 			std::cout << "Cam Rotation: [" << cam_rot.x << "," << cam_rot.y << "," << cam_rot.z << "]\n";
-			
+
 			m_cam->SetRotation(cam_rot);
 		}
 
