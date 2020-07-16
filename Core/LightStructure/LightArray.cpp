d33d37f1e40fda45318df69c29e25944b154c9bf
mathiasgam <mathias@gammelmark.org>
2020-07-16 15:08:02 +0200

    Added LightArray class for linear sampling of lights

---
+++ Core/LightStructure/LightArray.cpp
@@ -0,0 +1,30 @@
+#include "pch.h"
+#include "LightArray.h"
+
+namespace LSIS {
+
+	LightArray::LightArray()
+	{
+	}
+
+	LightArray::~LightArray()
+	{
+	}
+
+	void LightArray::AddLight(std::shared_ptr<Light> light)
+	{
+	}
+
+	void LightArray::Build()
+	{
+	}
+
+	void LightArray::Destroy()
+	{
+	}
+
+	void LightArray::SampleLights()
+	{
+	}
+
+}
