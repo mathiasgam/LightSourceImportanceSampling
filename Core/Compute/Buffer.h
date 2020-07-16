7330f57c516a78641e75a21836f18825e744927d
mathiasgam <mathias@gammelmark.org>
2020-07-16 17:14:56 +0200

    Added template class ObjectBuffer for handling typed buffers

--- Core/Compute/Buffer.h
+++ Core/Compute/Buffer.h
@@ -23,5 +23,24 @@ namespace LSIS::Compute {
 		size_t m_size;
 	};
 
+	template<class T>
+	class ObjectBuffer {
+		ObjectBuffer(cl_context context, size_t count) : m_buffer(context, sizeof(T)* count) {}
+		virtual ~ObjectBuffer(){}
+
+		void Write(cl_command_queue queue, const T* data, size_t offset, size_t count) {
+			m_buffer.Write(queue, std::static_pointer_cast<const void*>(data), sizeof(T) * offset, sizeof(T) * count);
+		}
+
+		void Read(cl_command_queue queue, T* datam, size_t offset, size_t count) {
+			m_buffer.Read(queue, std::static_pointer_cast<void*>(data), sizeof(T) * offset, sizeof(T) * count);
+		}
+
+		inline size_t GetSize() const { return m_buffer.GetSize(); }
+
+	private:
+		Buffer m_buffer;
+	};
+
 
 }
