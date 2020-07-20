dc6dc66964c72a5b582fda86304d16d35003078b
mathiasgam <mathias@gammelmark.org>
2020-07-20 15:54:51 +0200

    Added Texture classes

---
+++ Core/Compute/SharedCLGL/SharedTexture2D.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "glm.hpp"
+#include "CL/cl.hpp"
+
+#include "Compute/Compute.h"
+#include "Compute/Buffer.h"
+#include "Kernels/shared_defines.h"
+
+#include "Graphics/Texture.h"
+
+namespace LSIS::Compute {
+
+	class SharedTexture2D : public Texture {
+	public:
+		SharedTexture2D(const cl::Context& context, size_t width, size_t height);
+		virtual ~SharedTexture2D();
+
+		virtual void Bind() const override;
+		virtual void UnBind() const override;
+
+		void Update(const cl::CommandQueue& queue, const cl::Buffer& buffer);
+
+		static void LoadKernels();
+
+	private:
+		size_t m_width;
+		size_t m_height;
+
+		uint32_t m_texture_id;
+		cl_mem m_cl_shared_texture;
+
+		static cl::Kernel s_kernel;
+		static cl::Program s_program;
+	};
+
+}
