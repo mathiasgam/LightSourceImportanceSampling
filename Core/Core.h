cef77653d8cb4e61ebe845f6ef5a98a206d86b64
NOTEBOOK-XPS\mathi <mathiasgam@hotmail.com>
2020-09-22 08:03:33 +0200

    Fixed CORE_ERROR macro

--- Core/Core.h
+++ Core/Core.h
@@ -2,10 +2,11 @@
 
 #include <memory>
 
+#define CORE_ERROR(x, ...) { printf(x, __VA_ARGS__); __debugbreak(); }
 
 #ifdef ENABLE_ASSERTS
 	//#define LSIS_ASSERT(x, ...) { if (!(x)) { LOG_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
-	#define CORE_ASSERT(x, ...) { if (!(x)) { CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+	#define CORE_ASSERT(x, ...) { if (!(x)) { CORE_ERROR("Assertion Failed: %s", __VA_ARGS__); __debugbreak(); } }
 #else
 	//#define ASSERT(x, ...)
 	#define CORE_ASSERT(x, ...)
